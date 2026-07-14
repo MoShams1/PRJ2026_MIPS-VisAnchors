be525c8f57de197a79687c62cb9002150d61c0cd
Mohammad Shams <m.shamsahmar@gmail.com>
2026-07-14 16:04:05 +0200

    modified fig1A; added double conditions (fig1C); added eccentricity (fig1D)

--- exp1.py
+++ exp1.py
@@ -16,10 +16,6 @@ for file in all_files:
 
     all_data.append(data)
 
-# todo: modify the first plot DONE
-# todo: make a function to "clean" plots
-# todo: make plot for the two-dot conditions
-# todo: make plot for the eccentricity
 
 # ----------------------------------------------------------------------------
 # replica distance - all conditions
@@ -122,6 +118,122 @@ for subj in subjects_nvis:
 
 plt.axhline(y=6, color='black', linestyle="--", linewidth=1)
 plt.savefig("fig1B.svg")
+
+# ----------------------------------------------------------------------------
+# replica distance - two-dot configurations
+
+nsubj = len(all_data)
+cnd_dict = {
+    "Single": [1, 2, 3, 4],
+    "Orthogonal": [6, 8],
+    "Parallel": [5, 7],
+    "Diagonal": [9, 10],
+    "Full": [11]}
+
+cnd_list = list(cnd_dict.keys())
+
+subjects = []
+for isubj in range(nsubj):
+
+    subject = []
+    for _, meta_cnd_array in cnd_dict.items():
+        ind = np.isin(unique_cnds, meta_cnd_array)
+        subject.append(dist_cnd_mat[isubj, ind].mean())
+
+    subjects.append(subject)
+
+subjects_2vis = np.array(subjects)
+
+# ----------------------------------------------------------------------------
+# panel C
+
+plt.figure(figsize=(3.5, 4))
+ax = plt.gca()
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+
+subjects_2vis_m = subjects_2vis.mean(axis=0)
+subjects_2vis_sem = subjects_2vis.std(axis=0, ddof=1) / np.sqrt(nsubj)
+plt.bar(cnd_list[1:-1],
+        subjects_2vis_m[1:-1],
+        yerr=subjects_2vis_sem[1:-1],
+        color='lightgray')
+plt.xlabel("Extension type")
+plt.ylabel("Replica distance (dva)")
+
+for subj in subjects_2vis:
+    plt.scatter(
+        cnd_list[1:-1],
+        subj[1:-1],
+        edgecolor="none",
+        facecolor="black",
+        alpha=.3,
+        s=20
+    )
+
+plt.axhline(y=6, color='black', linestyle="--", linewidth=1)
+plt.axhline(y=float(subjects_2vis_m[0]), color='darkgray', linestyle="-",
+            linewidth=1)
+plt.axhline(y=float(subjects_2vis_m[-1]), color='darkgray', linestyle="-",
+            linewidth=1)
+plt.savefig("fig1C.svg")
+
+# ----------------------------------------------------------------------------
+# replica distance - eccentricity
+
+nsubj = len(all_data)
+cnd_dict = {
+    "Close1": [1, 2],
+    "Far1": [3, 4],
+    "Close2": [5],
+    "Far2": [7]
+}
+
+cnd_list = list(cnd_dict.keys())
+
+subjects = []
+for isubj in range(nsubj):
+
+    subject = []
+    for _, meta_cnd_array in cnd_dict.items():
+        ind = np.isin(unique_cnds, meta_cnd_array)
+        subject.append(dist_cnd_mat[isubj, ind].mean())
+
+    subjects.append(subject)
+
+subjects_ecc = np.array(subjects)
+
+# ----------------------------------------------------------------------------
+# panel D
+
+plt.figure(figsize=(4, 4))
+ax = plt.gca()
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+
+subjects_ecc_m = subjects_ecc.mean(axis=0)
+subjects_ecc_sem = subjects_ecc.std(axis=0, ddof=1) / np.sqrt(nsubj)
+x = [1, 2, 3.5, 4.5]
+plt.bar(x,
+        subjects_ecc_m,
+        yerr=subjects_ecc_sem,
+        color='lightgray')
+plt.xlabel("Eccentricity")
+plt.ylabel("Replica distance (dva)")
+
+for subj in subjects_ecc:
+    plt.scatter(
+        x,
+        subj,
+        edgecolor="none",
+        facecolor="black",
+        alpha=.3,
+        s=20
+    )
+plt.xticks(x, cnd_list)
+
+plt.axhline(y=6, color='black', linestyle="--", linewidth=1)
+plt.savefig("fig1D.svg")
 
 # ----------------------------------------------------------------------------
 plt.show()
